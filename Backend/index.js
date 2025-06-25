9d9f3aab80aff55205eb74b99ab7fd356168e19b
Abhi-boostin <abhimanyusinghworks@gmail.com>
2025-06-25 18:24:50 +0530

    "Initial backend setup with express, dotenv, nodemon, and .gitignore"

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("Stashboard backend is running!");
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
